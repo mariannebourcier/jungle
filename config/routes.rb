b021305053e2e5d80e58be7459fce3493cbfbb83
mariannebourcier <marianne.bourcier@gmail.com>
2022-09-06 14:40:00 -0600

    Add create users table, views

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   end
 
   get "/about", to: "about#show"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "/logout",  to: "sessions#destroy"
+
+  get "/signup",  to: "users#new"
+  post "/users",  to: "users#create"
 
   resources :orders, only: [:create, :show]
 
